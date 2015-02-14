a46a05b06af3ddc50289a053d87933a3f13090d9
Tomas-Muller <muller@unitime.org>
2015-02-14 16:54:05 +0100

    Online Student Scheduling: Eligibility Check
    - first implementation of REQ.04
    - in the custom elibility check, enrollment is disabled when used by a student that already has a schedule
    - added ability to provide custom message in the UTECStudentSectioningMessages file
    - set the RECHECK_AFTER_ENROLLMENT (recheck elibility after a succesful enrollment)
      - this is to hide the Submit Schedule button when a student is registered

---
+++ JavaSource/pe/edu/utec/unitime/resources/UTECStudentSectioningMessages.java
@@ -0,0 +1,14 @@
+package pe.edu.utec.unitime.resources;
+
+import org.unitime.timetable.gwt.resources.StudentSectioningMessages;
+
+public interface UTECStudentSectioningMessages extends StudentSectioningMessages {
+	@DefaultMessage("You are already registered.")
+	String failedEligibilityAlreadyRegistered();
+	
+	@DefaultMessage("Unrecognized response received.")
+	String failedEligibilityBadResponse();
+
+	@DefaultMessage("Unrecognized response received.")
+	String failedEnrollmentBadResponse();
+}
